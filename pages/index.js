6da8db2fa3418eaafdec7b1b1dcf1d9433644346
Victor Cruz <vicdan215@gmail.com>
2020-12-26 23:33:43 -0600

    Change of format

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,17 @@ export default function Home() {
         <div className="container mx-auto px-6">
           <div className="w-full lg:flex items-center">
             <div className="w-full lg:w-1/2">
-              <h2 className="text-md lg:text-2xl text-gray-600">Init this new proyect,</h2>
-              <h1 className="text-5xl lg:text-6xl font-bold text-yellow-500 mb-2 lg:mb-6">World ranks!</h1>
-              <p className="text-md lg:text-xl font-light text-gray-800 mb-8">Powerful analytics tools for your business. See the exact keywords for which your competitors rank in organic search and the amount of traffic driven by each of them.</p>
+              <h2 className="text-md lg:text-2xl text-gray-600">
+                Init this new proyect,
+              </h2>
+              <h1 className="text-5xl lg:text-6xl font-bold text-yellow-500 mb-2 lg:mb-6">
+                World ranks!
+              </h1>
+              <p className="text-md lg:text-xl font-light text-gray-800 mb-8">
+                Powerful analytics tools for your business. See the exact
+                keywords for which your competitors rank in organic search and
+                the amount of traffic driven by each of them.
+              </p>
             </div>
             <div className="w-full lg:w-1/2 lg:pl-24">
               <div className="bg-gray-100 shadow-sm rounded-md p-8">
@@ -22,7 +30,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-
     </div>
   );
 }
